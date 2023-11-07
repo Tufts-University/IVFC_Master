e8ad83430c1e627ec86896f14ae63b8f8cb3348e
TarasHanulia <taras.hanulia@tufts.edu>
2023-11-07 11:21:01 -0500

    <>

---
+++ ShellFiles/Data_Analysis_20231106_CAL27Cell_6PMT.m
@@ -0,0 +1,46 @@
+%% 110223_DataAnalysis
+% Owner: Taras Hanulia
+% Data Type: CAL27 Cell Data plus GFP
+% Flow Date: 11/06/23
+%% Notes
+% CAL27 Cells shorter slit
+%% Initialization
+clear
+clc
+addpath 'C:\Users\thanul01\Documents\MATLAB\ivfc_master'
+%% Calling Script
+
+    filepath = 'T:\Taras\IVFC\Acquired Data\Cell\TH_110623_Cell';
+    % Labview Conversion
+    Fs=60e3;
+    Window_Low= 50;
+    Window_High= 10000;
+    output=Labview_convert_rawdata_batch_6PMT(filepath,Fs,Window_Low,Window_High);
+    disp(output)
+    close all
+    
+    cd(filepath)
+    dirinfo = dir();
+    dirinfo(~[dirinfo.isdir]) = [];  %remove non-directories
+    dirinfo(ismember( {dirinfo.name}, {'.', '..'})) = [];  %remove . and ..
+    exp_name = dirinfo(1).name;
+    date = exp_name(4:9);
+    date = strread(date,'%2s');
+    %%Peak Detection
+    outputfile= ['NEW_peak_values_',date{1},'_',date{2},'_',date{3}];
+    dirinfo = dir('*Cell*');
+    dirinfo(~[dirinfo.isdir]) = [];  %remove non-directories
+    dirinfo(ismember( {dirinfo.name}, {'.', '..'})) = [];  %remove . and ..
+    file_range= (1:length(dirinfo));
+    analysisvals=[1,2,3,4];
+    sample_type= 'Cells';
+    exp_num=[];
+    std_threshold=3;
+    Spectralon_tail= '';
+    FWMH_threshold=0;
+    intensity_threshold= 0.1;
+    bead_flag=0;
+    output=SamplePeakDetection_PCA_PN_6PMT(filepath,outputfile,file_range,Window_Low,...
+        Window_High,Fs,analysisvals,sample_type,exp_num,std_threshold,...
+        Spectralon_tail,FWMH_threshold,intensity_threshold,bead_flag);
+    disp(output)
---
+++ ShellFiles/Data_Analysis_6PMT_20231106_Beads.m
@@ -0,0 +1,46 @@
+%% 11/06/23 DataAnalysis
+% Owner: Taras Hanulia
+% Data Type: Beads Calibration 
+% Flow Date: 11/06/2023 
+%% Experiment Note
+% short slit
+%% Initialization
+clear
+clc
+addpath 'C:\Users\thanul01\Documents\MATLAB\ivfc_master'
+%% Calling Script
+%% Bead Calibration
+% Labview Conversion
+filepath = 'T:\Taras\IVFC\Acquired Data\Bead Calibration Data\2023\6PMT\TH_110623_Calibration';
+Fs=60e3;
+Window_Low= 50;
+Window_High= 6000;
+output=Labview_convert_rawdata_batch_6PMT(filepath,Fs,Window_Low,Window_High);
+disp(output)
+close all
+%%
+%Peak Detection
+outputfile= 'NEW_peak_values_11_06_23';
+file_range= (1:2);
+analysisvals=(1:4);
+sample_type= 'Beads';
+exp_num=[];
+std_threshold=4;
+Spectralon_tail= '';
+FWMH_threshold=0;
+intensity_threshold= 0.15;
+bead_flag=0;
+output=SamplePeakDetection6PMT(filepath,outputfile,file_range,Window_Low,...
+    Window_High,Fs,analysisvals,sample_type,exp_num,std_threshold,...
+    Spectralon_tail,FWMH_threshold,intensity_threshold,bead_flag);
+disp(output)
+
+% Calibration
+files={
+    'NEW_peak_values_10_31_23';...
+    'NEW_peak_values_11_01_23';...
+    'NEW_peak_values_11_31_23';...
+    'NEW_peak_values_11_02_23';...
+    'NEW_peak_values_11_06_23'};
+output= DailyCalibrationScript6PMT(files);
+disp(output)
