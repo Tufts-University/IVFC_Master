82eef4697ec86bb3e187ed7703b34696bf466388
TarasHanulia <taras.hanulia@tufts.edu>
2023-11-28 17:57:49 -0500

    <new blood experiment>

---
+++ ShellFiles/Data_Analysis_6PMT_20231128Blood.m
@@ -0,0 +1,37 @@
+%% 11/28/23 DataAnalysis
+% Owner: Taras Hanulia
+% Data Type: Blood    Measurements
+% Flow Date: 11/28/2023
+%% Experiment Note
+% CAL27EGFP Cells 
+% flow faster than expected
+%% Initialization
+clear
+clc
+addpath 'C:\Users\thanul01\Documents\MATLAB\ivfc_masterr'
+%% Calling Script
+%% Bead Calibration
+% Labview Conversion
+filepath = 'T:\Taras\IVFC\Acquired Data\Blood Cell Data\TH_112823_Blood_Cell';
+Fs=60e3;
+Window_Low= 50;
+Window_High= 6000;
+output=Labview_convert_rawdata_batch_6PMT(filepath,Fs,Window_Low,Window_High);
+disp(output)
+close all
+%%
+%Peak Detection
+outputfile= 'NEW_peak_values_11_28_23';
+file_range= (1:7);
+analysisvals=[1,2,3,6];
+sample_type= 'Blood';
+exp_num=[];
+std_threshold=3;
+Spectralon_tail= '';
+FWMH_threshold=0;
+intensity_threshold= 0.15;
+bead_flag=1;
+output=SamplePeakDetection6PMTRF2(filepath,outputfile,file_range,Window_Low,...
+    Window_High,Fs,analysisvals,sample_type,exp_num,std_threshold,...
+    Spectralon_tail,FWMH_threshold,intensity_threshold,bead_flag);
+disp(output)
