98739ba4baf546b7e0c52157d6a9fc3951c88821
Taras Hanulia <taras.hanulia@tufts.edu>
2023-11-30 18:17:00 -0500

    <>

--- ShellFiles/Data_Analysis_6PMT_20231128Blood.m
+++ ShellFiles/Data_Analysis_6PMT_20231128Blood.m
@@ -16,8 +16,8 @@ filepath = 'T:\Taras\IVFC\Acquired Data\Blood Cell Data\TH_112823_Blood_Cell';
 Fs=60e3;
 Window_Low= 50;
 Window_High= 6000;
-output=Labview_convert_rawdata_batch_6PMT(filepath,Fs,Window_Low,Window_High);
-disp(output)
+% % output=Labview_convert_rawdata_batch_6PMT(filepath,Fs,Window_Low,Window_High);
+% disp(output)
 close all
 %%
 %Peak Detection
