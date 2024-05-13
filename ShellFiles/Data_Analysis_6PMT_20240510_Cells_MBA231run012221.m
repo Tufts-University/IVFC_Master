a1fd4a4ff5dd71000fb16046bd2bc3eaef206e8b
Taras Hanulia <taras.hanulia@tufts.edu>
2024-05-13 14:19:27 -0400

    <Cells Analysis on old data>

---
+++ ShellFiles/Data_Analysis_6PMT_20240510_Cells_MBA231run012221.m
@@ -0,0 +1,37 @@
+%% 05/10/23 DataAnalysis 
+% Owner: Taras Hanulia
+% Data Type: Cells   Measurements
+% Flow Date: 01/22/2021
+%% Experiment Note
+% MBA231 GFP+
+% Cells in Media
+%% Initialization
+clear
+clc
+addpath 'C:\Users\hanul\Documents\MATLAB\IVFC_Master'
+%% Calling Script
+%% Bead Calibration
+% Labview Conversion
+filepath = 'R:\Taras\IVFC\231GFP+ Cell Data\NV_012221_231GFP+';
+Fs=60e3;
+Window_Low= 50;
+Window_High= 6000;
+% output=Labview_convert_rawdata_batch_6PMT(filepath,Fs,Window_Low,Window_High);
+% disp(output)
+close all
+%%
+%Peak Detection
+outputfile= 'NEW_peak_values_05_10_24_RF';
+file_range= (1);
+analysisvals=[1,2,3];
+sample_type= 'Cells';
+exp_num=[];
+std_threshold=4;
+Spectralon_tail= '';
+FWMH_threshold=0;
+intensity_threshold= 0.1;
+bead_flag=0;
+output=SamplePeakDetection6PMTRF(filepath,outputfile,file_range,Window_Low,...
+    Window_High,Fs,analysisvals,sample_type,exp_num,std_threshold,...
+    Spectralon_tail,FWMH_threshold,intensity_threshold,bead_flag);
+disp(output)
