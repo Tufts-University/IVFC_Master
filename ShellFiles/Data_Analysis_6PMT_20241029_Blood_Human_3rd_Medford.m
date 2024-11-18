3fc6b181384bf33bf24920a86a1001a92601eaf2
Taras Hanulia <taras.hanulia@tufts.edu>
2024-11-18 09:35:57 -0500

    <>

--- ShellFiles/Data_Analysis_6PMT_20241029_Blood_Human_3rd_Medford.m
+++ ShellFiles/Data_Analysis_6PMT_20241029_Blood_Human_3rd_Medford.m
@@ -14,8 +14,8 @@ addpath 'C:\Users\thanul01\Documents\MATLAB\ivfc_master'
     Fs=60e3;
     Window_Low= 50;
     Window_High= 10000;
-    output=Labview_convert_rawdata_batch_6PMT(filepath,Fs,Window_Low,Window_High);
-    disp(output)
+%     output=Labview_convert_rawdata_batch_6PMT(filepath,Fs,Window_Low,Window_High);
+%     disp(output)
     close all
     
     cd(filepath)
@@ -26,11 +26,11 @@ addpath 'C:\Users\thanul01\Documents\MATLAB\ivfc_master'
     date = exp_name(4:9);
     date = strread(date,'%2s');
     %%Peak Detection
-    outputfile= ['NEW_peak_values_',date{1},'_',date{2},'_',date{3}];
+    outputfile= ['NEW_peak_values_',date{1},'_',date{2},'_',date{3},'_19_24'];
     dirinfo = dir('*Blood*');
     dirinfo(~[dirinfo.isdir]) = [];  %remove non-directories
     dirinfo(ismember( {dirinfo.name}, {'.', '..'})) = [];  %remove . and ..
-    file_range= (19:length(dirinfo)); %file_range= (3:length(dirinfo));
+    file_range= (19:24); %file_range= (3:length(dirinfo));
     analysisvals=[1,2,3,6];
     sample_type= 'Blood';
     exp_num=[];
@@ -38,7 +38,7 @@ addpath 'C:\Users\thanul01\Documents\MATLAB\ivfc_master'
     Spectralon_tail='';
     FWMH_threshold=0;
     intensity_threshold=0.1;
-    bead_flag=0;
+    bead_flag=1;
     output=SamplePeakDetection6PMTRF_PCA_PN(filepath,outputfile,file_range,Window_Low,...
         Window_High,Fs,analysisvals,sample_type,exp_num,std_threshold,...
         Spectralon_tail,FWMH_threshold,intensity_threshold,bead_flag);
